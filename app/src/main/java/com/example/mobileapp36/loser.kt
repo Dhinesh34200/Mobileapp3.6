3882ad64d9beb9f857f904bc50d1f4c192175623
Dhinesh Lingesan <dhinesh.lingesan@kanini.com>
2021-12-07 10:38:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mobileapp36/loser.kt
@@ -0,0 +1,11 @@
+package com.example.mobileapp36
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loser)
+    }
+}
---
+++ app/src/main/java/com/example/mobileapp36/winner.kt
@@ -0,0 +1,16 @@
+package com.example.mobileapp36
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class winner: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_winner)
+
+        val number = intent.getStringExtra("Number")
+        findViewById<TextView>(R.id.Number).text = "$number"
+
+    }
+}
